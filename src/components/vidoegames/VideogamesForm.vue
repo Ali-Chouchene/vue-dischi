15ca8103555303ff7b9c09651c76623a6c6bed05
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-20 17:44:42 +0100

    add checkbox

--- src/components/vidoegames/VideogamesForm.vue
+++ src/components/vidoegames/VideogamesForm.vue
@@ -16,9 +16,10 @@ export default {
             <textarea class="form-control" name="suggest" id="suggest" rows="3"></textarea>
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+            <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio1" value="option1">
             <label class="form-check-label" for="inlineRadio1">Iscrivti alla Newsletter!</label>
         </div>
+
         <div class="d-flex justify-content-end my-3">
             <button type="submit" class="btn btn-primary">INVIA</button>
         </div>
