ab98b1a1e9533bfe2133e033d4bdbffb89a3dd6a
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-20 17:39:22 +0100

    add btn

--- src/components/vidoegames/VideogamesForm.vue
+++ src/components/vidoegames/VideogamesForm.vue
@@ -8,18 +8,20 @@ export default {
     <!-- FORM -->
     <form action="">
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Inidirizzo Email</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="email" class="form-label">Inidirizzo Email</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Suggerisci</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="suggest" class="form-label">Suggerisci</label>
+            <textarea class="form-control" name="suggest" id="suggest" rows="3"></textarea>
         </div>
         <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
             <label class="form-check-label" for="inlineRadio1">1</label>
         </div>
-        <button>INVIA</button>
+        <div class="d-flex justify-content-end my-3">
+            <button type="submit">INVIA</button>
+        </div>
     </form>
 </template>
 
