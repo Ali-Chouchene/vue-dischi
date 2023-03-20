a2097d52cec84c1165ccbaa217d7c650680cc71c
Ali-Chouchene <choucheneali9801@gmail.com>
2023-03-20 16:26:03 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// PAGES
+
+
+// ROUTES
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+
+    ]
+});
+
+export { router };
