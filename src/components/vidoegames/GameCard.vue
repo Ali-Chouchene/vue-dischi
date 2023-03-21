085d3298a3d55bffa22842cad0d514f0d012439b
Ali-Chouchene <choucheneali9801@gmail.com>
2023-03-21 15:49:13 +0100

    game card done

---
+++ src/components/vidoegames/GameCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'GameCard',
+    props: { game: Object }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="game.image" :src="game.image" class="card-img-top" :alt="game.slug">
+        <div class="card-body">
+            <h5 class="card-title">{{ game.title }}</h5>
+            <p class="card-text">{{ game.description }}</p>
+            <p>{{ game.price }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
--- src/components/vidoegames/GamesList.vue
+++ src/components/vidoegames/GamesList.vue
@@ -1,22 +1,33 @@
 <script >
+import axios from 'axios';
+import GameCard from '../vidoegames/GameCard.vue';
+const apiUri = 'http://127.0.0.1:8000/api/videogames';
 export default {
     name: 'GamesList',
+    components: { GameCard },
+    data: () => ({
+        videogames: [],
+    }),
+    methods: {
+        fetchVideogames() {
+            axios.get(apiUri).then(res => {
+                this.videogames = res.data;
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    },
+    created() {
+        this.fetchVideogames();
+    }
 };
 </script>
 <template>
     <div class="container my-4">
         <h2 class="mb-4">All Games</h2>
         <div class="row g-4 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
-            <div v-for="index in 21" class="col">
-                <div class="card">
-                    <img src="http://via.placeholder.com/600x500" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+            <div v-for="game in videogames" :key="game.id" class="col">
+                <GameCard :game="game" />
             </div>
         </div>
     </div>
