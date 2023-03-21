afd9b04dc384a269e7b990f7a02c0b5b3171268e
Ali-Chouchene <choucheneali9801@gmail.com>
2023-03-21 17:35:08 +0100

    gamecard fixed

--- src/components/vidoegames/GameCard.vue
+++ src/components/vidoegames/GameCard.vue
@@ -8,11 +8,76 @@ export default {
 <template>
     <div class="card h-100">
         <img v-if="game.image" :src="game.image" class="card-img-top" :alt="game.slug">
-        <div class="card-body">
+        <div class="card-body m-0 p-0">
             <h5 class="card-title">{{ game.title }}</h5>
-            <p class="card-text">{{ game.description }}</p>
-            <p>{{ game.price }}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <!-- <p class="card-text">{{ game.description }}</p> -->
+            <span class="card-price">{{ game.price }}</span>
+            <a id="card-button" href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </template>
+<style lang="scss" scoped>
+.card {
+    position: relative;
+    height: 240px;
+    box-shadow: 5px 2px 15px 0px rgba(0, 0, 0, 0.75);
+    border: 0;
+    cursor: pointer;
+
+    &:hover {
+        border: 1px solid blue;
+    }
+
+
+
+
+    .card-title {
+        position: absolute;
+        top: 20px;
+        left: 10px;
+    }
+
+    #card-button {
+        position: absolute;
+        bottom: -15px;
+        right: -15px;
+    }
+
+    .card-price {
+        height: 50px;
+        width: 50px;
+        position: absolute;
+        bottom: 7px;
+        left: 5px;
+        background-color: rgb(255, 0, 0);
+        color: #ffffff;
+        border-radius: 50%;
+        padding: 8px 2px;
+        display: block;
+        font-size: 12px;
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &:hover {
+            background-color: rgb(17, 0, 255);
+        }
+
+
+    }
+}
+
+img {
+    height: 100%;
+    object-fit: cover;
+    // width: 450px;
+}
+
+h5 {
+    text-shadow: 0px 0px 25px rgba(255, 0, 0, 0.79);
+    color: white;
+    font-size: 1.5rem;
+
+}
+</style>
--- src/components/vidoegames/GamesList.vue
+++ src/components/vidoegames/GamesList.vue
@@ -26,11 +26,11 @@ export default {
     <div class="container my-4">
         <h2 class="mb-4">All Games</h2>
         <div class="row g-4 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
-            <div v-for="game in videogames" :key="game.id" class="col">
+            <div v-for="game in videogames" :key="game.id" class="col mb-3">
                 <GameCard :game="game" />
             </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
