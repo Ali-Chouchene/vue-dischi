311fa545fa59f73d8ef4627b9ce04eba15233970
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-22 17:38:19 +0100

    fix form

--- src/components/vidoegames/VideogamesForm.vue
+++ src/components/vidoegames/VideogamesForm.vue
@@ -8,16 +8,27 @@ export default {
     <!-- FORM -->
     <form action="">
         <div class="mb-3">
-            <label for="email" class="form-label">Inidirizzo Email</label>
-            <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+            <label for="email" class="form-label">Inidirizzo Email<sup class="text-danger">*</sup> </label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
+            <small class="text-muted form-text">La tua email. Sarai ricontattato a questo indirizzo</small>
         </div>
+        <!-- oggetto -->
+
         <div class="mb-3">
-            <label for="suggest" class="form-label">Suggerisci</label>
-            <textarea class="form-control" name="suggest" id="suggest" rows="3"></textarea>
+            <label for="subject" class="form-label">Oggetto della mail<sup class="text-danger">*</sup></label>
+            <input class="form-control" name="subject" id="subject" rows="3">
         </div>
+        <!-- contenuto -->
+
+        <div class="mb-3">
+            <label for="message" class="form-label">Contenuto del messaggio<sup class="text-danger">*</sup></label>
+            <textarea class="form-control" name="message" id="message" rows="5"></textarea>
+        </div>
+        <!-- check -->
+
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">Iscrivti alla Newsletter!</label>
+            <input class="form-check-input" type="checkbox" name="subscription" id="subscription" value="1">
+            <label class="form-check-label" for="subscription">Iscrivti alla Newsletter!</label>
         </div>
 
         <div class="d-flex justify-content-end my-3">
